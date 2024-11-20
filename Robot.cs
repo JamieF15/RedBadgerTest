fb349f624a82c514314ef3cc2d0ba81c1ecad874
Jamie Foster <jamiefoster15@hotmail.co.uk>
2024-11-20 17:37:26 +0000

    First commit with files

---
+++ Robot.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace RedBadgerForms
+{
+    internal class Robot
+    {
+        string orientation;
+        private Point cood;
+
+        public void Move(string direction)
+        {
+        
+            
+        }
+    }
+}
+
+
+
+
+
+
